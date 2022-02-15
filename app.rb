16bb12fc26b419fe347d6744304a9c6fea25febe
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-02-15 10:39:31 +0000

    App file setup

---
+++ app.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "capybara"
+require "capybara/rspec"
+require "rspec"
+require 'sinatra/reloader' if development?
+require "pg"
+
+
+class MakersBnB < Sinatra::Base
+
+  enable :sessions
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'test page'
+  end
+
+  run! if app_file == $0
+end
