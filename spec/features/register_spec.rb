de6a393ee7d4c47d127b528373753a705d12db3e
David <dave_carroll82@hotmail.com>
2022-02-15 16:39:49 +0000

    name added to database

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -3,4 +3,13 @@ feature "enable to user to sign up" do
     visit ('/Register')
     expect(page).to have_content "Register"
   end
+
+  scenario "The user submits name" do
+    visit "/"
+    click_link "Click here to register"
+    fill_in("user_name", with: "Dave")
+    click_button('Submit')
+
+    expect(page).to have_content "Hello User"
+  end
 end
