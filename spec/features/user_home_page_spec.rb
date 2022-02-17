46f4cc5126e9c3f68708589e25a2d0e6602e5688
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-02-17 12:01:19 +0000

    Added users_ref id to space in test

--- spec/features/user_home_page_spec.rb
+++ spec/features/user_home_page_spec.rb
@@ -3,4 +3,11 @@ feature 'User home page page' do
     login_test_user
     expect(page).to have_content("Current spaces:")
   end
+
+  scenario "can add an available date" do
+    login_test_user
+    fill_in("date", with: "2022-02-20")
+    click_button("submit")
+  end
+
 end
