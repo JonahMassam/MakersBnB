449976aa1bcfde711ff9abe50d9f6fb4a44b7936
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-02-17 16:39:26 +0000

    List out available spaces

---
+++ spec/features/request_available_space_spec.rb
@@ -0,0 +1,16 @@
+feature "Request available space" do
+  scenario "can visit page" do
+    login_test_user
+    visit ('/request_available_space')
+    expect(page).to have_content "Hello"
+  end
+
+  scenario "can submit a date" do
+    login_test_user
+    visit ('/request_available_space')
+    fill_in("request_date", with: "2022-03-14" )
+    click_button("submit")
+    expect(page).to have_content "Here are the available space on that date:"
+  end
+
+end
